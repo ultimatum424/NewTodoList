9486d0d855f86299af53739e2706ae501a112668
ultimatum424 <Good20032014>
2017-04-15 18:18:06 +0300

    Change colors

--- app/src/main/java/com/example/ultim/newtodolist/MainActivity.java
+++ app/src/main/java/com/example/ultim/newtodolist/MainActivity.java
@@ -25,8 +25,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
         mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         mRecyclerView.setHasFixedSize(true);
         mLayoutManager = new LinearLayoutManager(this);
